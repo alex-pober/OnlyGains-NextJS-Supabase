b3da6070d56dafe60d154384e24352d35b0f3998
Alex <alex.pober.95@gmail.com>
2023-11-17 22:01:53 -0800

    tabs for workouts fixed

--- app/workouts/workouts.tsx
+++ app/workouts/workouts.tsx
@@ -4,7 +4,7 @@ import WorkoutDay from "./workout-day";
 import NoWorkouts from "./no-workouts";
 import CreateWorkoutModal from "./create-workout-modal";
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 type Workout = {
   auth_id: string | null;
@@ -15,44 +15,42 @@ type Workout = {
   title: string;
 };
 
-export default function Workouts({ workoutsData, workoutCount, session }: { workoutsData: any, workoutCount: any, session: any }) {
+export default function Workouts({
+  workoutsData,
+  workoutCount,
+  session,
+}: {
+  workoutsData: any;
+  workoutCount: any;
+  session: any;
+}) {
   const [activeTab, setActiveTab] = useState<number>(
     workoutsData[0]?.id || null
   );
-
   return (
     <>
-      <Tabs defaultValue="account" className="w-[400px]">
-        <TabsList>
-          <TabsTrigger value="account">Account</TabsTrigger>
-          <TabsTrigger value="password">Password</TabsTrigger>
+      <Tabs defaultValue={activeTab.toString()}>
+        <TabsList className="flex w-min m-auto">
+          {workoutsData?.map((item: Workout) => {
+            return (
+              <TabsTrigger
+                key={item.id}
+                value={item.id.toString()}
+                onClick={() => setActiveTab(item.id)}
+              >
+                {item.title}
+              </TabsTrigger>
+            );
+          })}
         </TabsList>
-        <TabsContent value="account">Make changes to your account here.</TabsContent>
-        <TabsContent value="password">Change your password here.</TabsContent>
+        <TabsContent className="flex flex-col items-center" value={activeTab.toString()}>
+          {workoutsData.length > 0 ? (
+            <WorkoutDay workoutId={activeTab} />
+          ) : (
+            <NoWorkouts />
+          )}
+        </TabsContent>
       </Tabs>
-      <div className="tabs tabs-boxed mb-4">
-        {workoutsData?.map((item: Workout) => {
-          return (
-            <a
-              key={item.id}
-              className={
-                activeTab === item.id
-                  ? "tab tab-active transition-transform"
-                  : "tab transition-all"
-              }
-              onClick={() => setActiveTab(item.id)}
-            >
-              {item.title}
-            </a>
-          );
-        })}
-        <CreateWorkoutModal workoutCount={workoutCount} session={session}/>
-      </div>
-      {workoutsData.length > 0 ? (
-        <WorkoutDay workoutId={activeTab} />
-      ) : (
-        <NoWorkouts />
-      )}
     </>
   );
 }
